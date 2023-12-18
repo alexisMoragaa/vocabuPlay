8e55aa805494b06a4c8b7d104d69675fe2afa066
Alexis <alexis.moraga.gallardo@gmail.com>
2023-12-18 00:35:20 -0300

    -Se modifica el loop para gestionar el volumen desde una funcion externa -Se añade el codigo para leer una tarjeta rfid e imprimir su valor mediante el puerto serial

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,22 @@ void printDetail(uint8_t type, int value);
 //Variables para controlar la reprocuccion y volumen 👆
 
 
+//Variables para controlar el modulo RFID 👇
+#define RST_PIN 9 
+#define SS_PIN  10
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+//Variables para controlar el modulo RFID 👆
+
+
+//Variables para gestionar la lectura de la tarjeta 👇
+#define ELEMENTOS_TARJETA 3
+#define MAX_LARGO_TARJETA 10
+
+// char valoresTarjeta[ELEMENTOS_TARJETA][MAX_LARGO_TARJETA];
+// char* valoresTarjeta[ELEMENTOS_TARJETA];
+//Variables para gestionar la lectura de la tarjeta 👆
+
+
 bool preguntasSeleccionadas[500] =  {false};
 int getRamdomQuestion();//inicializamos la funcion que retorna una pregunta de forma aleatoria
 bool checkPlayStatus(); //inicializamos la funcion que determina el status de reproducción
@@ -29,18 +45,23 @@ int randomQuestion;
 const int nextQuestion = 3; //usamos el pin 3 para leer el boton que cambiara la pregunta👈
 int contadorPreguntasSeleccionadas = 0;//inicializamos el contador de preguntas respondidas
 
+void controlarVolumen(); //Declaramos la funcion que se usara en el control de volumen 👈
+String leerTarjetas(); //Declaramos la funcion que gestionara la lectura de tarjetas
 
 void setup() {
+  //Itera el numero de preguntas existentes en la matriz para establecer el largo final del arreglo
   for (int i = 0; i < tamano; i++) {
         preguntasSeleccionadas[i] = false;
   }
-  Serial.begin(9600);                                           // Initialize serial communications with the PC, COMMENT OUT IF IT FAILS TO PLAY WHEN DISCONNECTED FROM PC
+
+  Serial.begin(9600);
   mySoftwareSerial.begin(9600);
   SPI.begin();
+  mfrc522.PCD_Init();   
 
   pinMode(nextQuestion, INPUT_PULLUP);
 
-  Serial.println(F("Inicializando DFPlayer ... (Esto puede tomar algunos segundos)"));
+  Serial.println(F("Inicializando DFPlayer... "));
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
     Serial.println(F("Error al inizar DFPlayer mini"));
   }
@@ -58,15 +79,8 @@ void setup() {
 
 
 void loop() {
-  //Control del volumen 👇
-  volumen = map(analogRead(volumenPot), 0, 1023, 0, 30);  
-  if (volumen - prevVolumen >= 3 || prevVolumen - volumen >= 3 ){
-    myDFPlayer.volume(volumen);
-    Serial.println(volumen);  
-    prevVolumen = volumen;
-    delay(1);
-  }
-  //Control del volumen 👆
+
+  controlarVolumen(); //Gestiona el volumen
   
   //Cambiamos a la siguiente pregunta solo cuando la reproduccion de la pregunta anterior finalice
   if(digitalRead(nextQuestion) == LOW){
@@ -79,10 +93,22 @@ void loop() {
     }
   }
 
-
+  
+  String datosTarjeta =  leerTarjetas();// Lee las tarjetas
+  if (datosTarjeta != "") {
+      Serial.println(datosTarjeta);
+    }
 }
 
 
+void controlarVolumen() {
+  volumen = map(analogRead(volumenPot), 0, 1023, 0, 30);
+  if (abs(volumen - prevVolumen) >= 3) {
+    myDFPlayer.volume(volumen);
+    Serial.println(volumen);
+    prevVolumen = volumen;
+  }
+}
 
 //creamos una funcion que retorna un numero aleatorio de pregunta
 int  getRamdomQuestion(){
@@ -114,3 +140,56 @@ bool checkPlayStatus() {
   return isPlaying;
 }
 
+
+String leerTarjetas() {
+
+  MFRC522::MIFARE_Key key;
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  byte block, len;
+  MFRC522::StatusCode status;
+
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    Serial.println(F("**Card Detected:**"));
+    // mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid));
+
+    byte buffer2[18];
+    block = 1;
+    len = 18;
+
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("Authentication failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      delay(1000);
+      mfrc522.PICC_HaltA();
+      mfrc522.PCD_StopCrypto1();
+      return "";
+    }
+
+    status = mfrc522.MIFARE_Read(block, buffer2, &len);
+    if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("Reading failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      delay(1000);
+      mfrc522.PICC_HaltA();
+      mfrc522.PCD_StopCrypto1();
+      return "";
+    }
+
+    String lectura = "";
+    for (uint8_t i = 0; i < 16; i++) {
+      if (buffer2[i] != 0) {
+        lectura += (char)buffer2[i];
+      }
+    }
+
+    delay(200);
+    mfrc522.PICC_HaltA();
+    mfrc522.PCD_StopCrypto1();
+
+    return lectura;
+  }
+  return "";
+
+}
